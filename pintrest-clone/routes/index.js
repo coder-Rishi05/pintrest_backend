4429c19ca7e3bd024c36394cfd9d5327501fee3c
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-09-23 18:58:58 +0530

    passport-error

--- pintrest-clone/routes/index.js
+++ pintrest-clone/routes/index.js
@@ -1,61 +1,53 @@
-var express = require("express");
-var router = express.Router();
+
+const express = require("express");
+const router = express.Router();
 const userModel = require("./users");
 const Post = require("./posts");
 const passport = require("passport");
-const localStrategy = require('passport-local')
+const localStrategy = require("passport-local");
+
+// Passport setup
+passport.use(new localStrategy(userModel.authenticate()));
+passport.serializeUser(userModel.serializeUser());
+passport.deserializeUser(userModel.deserializeUser());
 
 router.get("/", (req, res) => {
   res.render("index", { title: "Express" });
 });
 
+router.get("/profile", (req, res) => {
+  res.render("profile");
+});
+
 router.post("/register", (req, res) => {
-  // const userData  = new userModel({
-  //    userName: req.body.userName,
-  //     email:req.body.email,
-  //     fullName:req.body.fullName,
-  // });
   const { userName, email, fullname } = req.body;
   const userData = new userModel({ userName, email, fullname });
 
   userModel.register(userData, req.body.password).then(() => {
-    passport.authenticate("local", () => {
-      res.redirect("/profile");
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("profile");
     });
   });
 });
 
-// router.get("/createuser", async (req, res) => {
-//   try {
-//     const created_user = await userModel.create({
-//       userName: "rishi21",
-//       password: "12321",
-//       posts: [],
-//       email: "rishi3@gmail.com",
-//       fullName: "rishirawat",
-//     });
-//     res.send(created_user);
-//   } catch (err) {
-//     res.send(err);
-//   }
-// });
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/",
+  })
+);
 
-// router.get("/alluserPost", async (req, res) => {
-//   let user = await userModel
-//     .findOne({ _id: "68cf63c3c783c0bd13d11088" })
-//     .populate("posts");
-//   res.send(user);
-// });
+router.get("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
 
-// router.get("/createpost", async (req, res) => {
-//   const postData = await Post.create({
-//     postText: "lovely image",
-//     user: "68cf63c3c783c0bd13d11088", // now this post belongs to the user who have this id.
-//   });
-//   let user = await userModel.findOne({ _id: "68cf63c3c783c0bd13d11088" });
-//   user.posts.push(postData._id);
-//   await user.save();
-//   res.send("created post and connected user and posts");
-// });
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/");
+}
 
 module.exports = router;
--- pintrest-clone/routes/users.js
+++ pintrest-clone/routes/users.js
@@ -1,3 +1,45 @@
+// const mongoose = require("mongoose");
+// const plm = require("passport-local-mongoose");
+
+// mongoose.connect("mongodb://127.0.0.1:27017/pintrestApp");
+
+// const userSchema = mongoose.Schema({
+//   userName: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+//   posts: [
+//     {
+//       type: mongoose.Schema.Types.ObjectId, //this is basically  id, in mongoose the id is defined as Types.ObjectId.
+//       ref: "Post", // the Post here is basically my refrence to The Post model.
+//     },
+//   ],
+//   dp: {
+//     type: String, // assume the profile pic is stored as a URL or file path.
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   fullname: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+// // userSchema.pluggin(plm);
+
+// const userModel = mongoose.model("User", userSchema); // here user means the name of the model in mongoDB.
+// userModel.pluggin(plm);
+
+// module.exports = userModel;
+
 const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 
@@ -9,19 +51,14 @@ const userSchema = mongoose.Schema({
     required: true,
     unique: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
+  password: String, // passport-local-mongoose will handle hashing
   posts: [
     {
-      type: mongoose.Schema.Types.ObjectId, //this is basically  id, in mongoose the id is defined as Types.ObjectId.
-      ref: "Post", // the Post here is basically my refrence to The Post model.
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
     },
   ],
-  dp: {
-    type: String, // assume the profile pic is stored as a URL or file path.
-  },
+  dp: String,
   email: {
     type: String,
     required: true,
@@ -33,8 +70,8 @@ const userSchema = mongoose.Schema({
   },
 });
 
-userSchema.pluggin(plm);
+// Correct plugin usage
+userSchema.plugin(plm);
 
-const userModel = mongoose.model("User", userSchema); // here user means the name of the model in mongoDB.
-
+const userModel = mongoose.model("User", userSchema);
 module.exports = userModel;
